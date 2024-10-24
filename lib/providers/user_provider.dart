18b7e595bd45efbae66b73ed4c8d27d6b30116e1
prabesh-stha <shresthaprabesh155@gmail.com>
2024-10-24 23:17:40 +0545

    Added User Collection in the firestore

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:messaging_app/models/user.dart';
+import 'package:messaging_app/providers/auth_provider.dart';
+
+final userProvider = StreamProvider.autoDispose<User?>((ref){
+  final user = ref.watch(authProvider).value;
+
+  if(user != null){
+    return FirebaseFirestore.instance.collection("users").doc(user.uid).snapshots().map((doc){
+      return User.fromFirestore(doc);
+    });
+  }else{
+    return Stream.value(null);
+  }
+});
---
+++ lib/services/user_services.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:messaging_app/models/user.dart';
+
+class UserServices {
+ static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  static const String collection = "users";
+
+  static Future<void> createUser(User user) async{
+    DocumentReference doc = _firestore.collection(collection).doc(user.uid);
+   await doc.set({
+      'uid' : user.uid,
+      'name' : user.name,
+      'email' : user.email
+    });
+  }
+}
